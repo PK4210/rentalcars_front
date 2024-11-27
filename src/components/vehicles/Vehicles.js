0327258664ea65d8424afef74d8c77be2a4a8652
PK <pedrojugador2@hotmail.com>
2024-11-27 00:39:35 -0300

    porqueeeeeeee

--- src/components/vehicles/Vehicles.js
+++ src/components/vehicles/Vehicles.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getVehicles } from '../../services/vehicleService';
-import VehicleCards from ''
+import VehiclesCard from './VehiclesCard';
 import './Vehicles.css';
 
 const Vehicles = () => {
